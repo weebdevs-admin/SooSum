30c9a4b7166c1f488b6d7de1e1362372204a82f6
weebhacker <weebhacker1@gmail.com>
2023-02-15 07:23:04 +0300

    add files

--- js/data.js
+++ js/data.js
@@ -81,10 +81,174 @@
             lanUZ: "Foyda",
             lanEN: "Benefit",
             lanRU: "Выгода"
+        },
+        
+
+
+
+    ],
+    
+    main:[
+        {
+            id:1,
+            lanUZ: "Qizil jen shen  1 199 000",
+            lanEN: "Red Jen Shen 1,199,000",
+            lanRU: "Рэд Джен Шен 1 199 000",
+        },
+        {
+            id:2,
+            lanUZ: "Oddiy jen shen  1 199 000",
+            lanEN: "Normal jen shen 1,199,000",
+            lanRU: "Обычный джен шен 1 199 000",
         }
-
-
-
+    ],
+    about:[
+        {
+            id:1,
+            lanUZ: "Qizil jenshen bilan oddiy jenshenni farqi,yetishtirilishi.Jenshen mahsulotini ekishdan oldin ekin maydoni 2 yil og'itlar bilan sugorilib tayyorlanadi. 3-yili esa yerga jenshen 2 xil usulda ekiladi,1-jenshen niholini donalab ekish(koreada asosan shu usuldan foydalaniladi),2- urugni sepish orqali. Yerga jenshen ekilgandan song, 1 ~ 8 yil davomida jenshen turli og'itlar bilan sugorilib katta qilinadi.(jenshen yerda qancha turganiga qarab oshancha yillik jenshen deb nomlanadi). Yerdan yangi uzilgan 1~8 yillik ho'l jenshen(koreyschada insam인삼,susam수삼 deb nomlanadi)bu oddiy jenshen hisoblanadi(lekin buniham foydali hususiyatlari juda kop. Giyohlar ichida qizil jenshendan song birinchi orinda turadi)",
+            lanEN: "Difference between red ginseng and common ginseng, cultivation.Before planting ginseng products, the field is prepared by irrigating with fertilizers for 2 years. In the 3rd year, ginseng is planted on the ground in 2 different ways, 1- by sowing ginseng seedlings in batches (this method is mainly used in Korea), 2- by sowing seeds. After ginseng is planted on the ground, ginseng is grown by watering it with various fertilizers for 1~8 years. 1~8-year-old wet ginseng freshly cut from the ground (called insam-insam, sesamsusam in Korean) is a common ginseng (but it also has many useful properties. Among herbs, it ranks first after red ginseng)",
+            lanRU: "Разница между красным женьшенем и обыкновенным женьшенем, выращивание.Перед посадкой продукции женьшеня поле подготавливают путем орошения удобрениями в течение 2 лет. На 3-й год женьшень высаживают в грунт 2-мя разными способами, 1- посевом рассады женьшеня партиями (этот способ в основном используется в Корее), 2- посевом семян. После того, как женьшень посажен на землю, женьшень выращивают, поливая его различными удобрениями в течение 1-8 лет. Свежесобранный с земли влажный женьшень возрастом от 1 до 8 лет (называемый по-корейски инсам-инсам, сезамсусам) — это обычный женьшень (но он также обладает многими полезными свойствами. Среди трав он занимает первое место после красного женьшеня)",
+        }
+    ],
+    about2:[
+        {
+            id:1,
+            lanUZ: "Kompaniya haqida",
+            lanEN: "About the company",
+            lanRU: "О компании",
+        },
+        {
+            id:2,
+            lanUZ: "Soosum kompaniyasi 2021-yildan boshlab uzoq sharq osimligi bolgan Jenshenning ildizidan tayyorlangan ichimlikni ishlab chiqarib, Ozbekiston boylab yetkazib berishni yolga qoydi. Kompaniyamiz Koreyaning Chejun ITD hamda Qizil Jenshen sharbati MChJ korxonalari bilan hamkorlikda ishlaydi. Sunmee qizil jenshen ildizi Janubiy Koreyada yetishtirilib Ozbekistonda qadoqlanadi.",
+            lanEN: "Starting from 2021, Soosum company has started producing a drink made from the root of the Far Eastern plant Ginseng and delivering it throughout Uzbekistan. Our company works in cooperation with Koreas Chejun ITD and Red Ginseng Juice LLC. Sunmee red ginseng root is grown in South Korea and packed in Uzbekistan.",
+            lanRU: "Начиная с 2021 года компания Soosum планирует производить напиток из корня женьшеня, произрастающего на Дальнем Востоке, и доставлять его по всему Узбекистану. Наша компания работает в сотрудничестве с предприятиями Jejun ITD и Red Ginseng Juice LLC Кореи. Корень красного женьшеня Sunmee выращивают в Южной Корее и упаковывают в Узбекистане.",
+        },
+    ],
+    footer:[
+        {
+            id:1,
+            lanUZ: "Koreya qizil jensheni ichimligini kimlar iste'mol qiladilar?",
+            lanEN: "Who Drinks Korean Red Ginseng Drink?",
+            lanRU: "Кто пьет корейский напиток из красного женьшеня?",
+        },
+        {
+            id:2,
+            lanUZ: "O’zida doimiy charchoq va holsizlik xis qiladiganlar",
+            lanEN: "Those who feel constant fatigue and weakness",
+            lanRU: "Тем, кто чувствует постоянную усталость и слабость",
+        },
+        {
+            id:3,
+            lanUZ: "Saraton kasalligini oldini olishni istaganlar",
+            lanEN: "Those who want to prevent cancer",
+            lanRU: "Тем, кто хочет предотвратить рак",
+        },
+        {
+            id:4,
+            lanUZ: "Aqliy va jismoniy faoliyati pasayganlar",
+            lanEN: "Those with reduced mental and physical activity",
+            lanRU: "Лица со сниженной умственной и физической активностью",
+        },
+        {
+            id:5,
+            lanUZ: "Bemorlikdan keyin oyoqqa tezroq turishni xohlaganlar",
+            lanEN: "Those who want to get back on their feet faster after illness",
+            lanRU: "Тем, кто хочет быстрее встать на ноги после болезни",
+        },
+        {
+            id:6,
+            lanUZ: "Jinsiy quvvati kamligidan aziyat chekadiganlar",
+            lanEN: "Those who suffer from low libido",
+            lanRU: "Тем, кто страдает пониженным либидо",
+        },{
+            id:7,
+            lanUZ: "Qarilikni sekinlashtirishni istaganlar",
+            lanEN: "Those who want to slow down aging",
+            lanRU: "Тем, кто хочет замедлить старение",
+        },{
+            id:8,
+            lanUZ: "Tana va bo’g’imlarda doimiy og’riq xis qiladiganlar",
+            lanEN: "Those who feel constant pain in the body and joints",
+            lanRU: "Тем, кто испытывает постоянную боль в теле и суставах",
+        },
+    ],
+    footer2:[
+        {
+            id:1,
+            lanUZ: "Qizil jenshen ildizi ichimligidan qanday foydalaniladi?",
+            lanEN: "How to use red ginseng root drink?",
+            lanRU: "Как использовать напиток из корня красного женьшеня?",
+        },
+        {
+            id:2,
+            lanUZ: "Qizil jenshen bilan oddiy jenshenni farqi,yetishtirilishi.Jenshen mahsulotini ekishdan oldin ekin maydoni 2 yil og'itlar bilan sugorilib tayyorlanadi. 3-yili esa yerga jenshen 2 xil usulda ekiladi,1-jenshen niholini donalab ekish(koreada asosan shu usuldan foydalaniladi),2- urugni sepish orqali. Yerga jenshen ekilgandan song, 1 ~ 8 yil davomida jenshen turli og'itlar bilan sugorilib katta qilinadi.(jenshen yerda qancha turganiga qarab oshancha yillik jenshen deb nomlanadi). Yerdan yangi uzilgan 1~8 yillik ho'l jenshen(koreyschada insam인삼,susam수삼 deb nomlanadi)...*rasm qo'y* bu oddiy jenshen hisoblanadi(lekin buniham foydali hususiyatlari juda kop. Giyohlar ichida qizil jenshendan song birinchi orinda turadi). Yerdan yangi uzilgan 1~8 yillik jenshenni zavodda yuvib tozalab ortiqcha shoxlarini qirqib, zavoddagi mahsus bosimli quritish tehnologiyasida quritilgan jenshen( quritilgan jenshen koreyschada 홍삼 xo'ngsam deb ataladi, quriganida qizil tusga kiradi va foydali hususiyatlari yanada ortadi, shuning uchunham bunga yetadigan giyoh yoq va dunyoda giyohlar qiroli hisoblanadi)* rasm qo'y*qizil jenshen deb ataladi.",
+            lanEN: "The difference between red ginseng and common ginseng, cultivation. Before planting ginseng products, the field is irrigated with fertilizers for 2 years. In the 3rd year, ginseng is planted on the ground in 2 different ways, 1- by sowing ginseng seedlings in batches (this method is mainly used in Korea), 2- by sowing seeds. After ginseng is planted on the ground, ginseng is grown by watering it with various fertilizers for 1~8 years. 1~8-year-old wet ginseng freshly cut from the ground (insam-insam, sesamsusam in Korean)...*insert picture* is considered ordinary ginseng (but it also has many useful properties. Among herbs, it ranks first after red ginseng ). 1~8-year-old ginseng freshly harvested from the ground is washed and cleaned at the factory, the excess branches are cut off, and the ginseng is dried using a special pressure drying technology at the factory (dried ginseng is called hongsam hongsam in Korean, when it dries, it turns red and its useful properties increase even more, so it is enough to drink and is considered the king of herbs in the world)* the picture is called red ginseng.",
+            lanRU: "Разница между красным женьшенем и женьшенем обыкновенным, выращивание.Перед посадкой продуктов из женьшеня поле поливают удобрениями в течение 2 лет. На 3-й год женьшень высаживают в грунт 2-мя разными способами, 1- посевом рассады женьшеня партиями (этот способ в основном используется в Корее), 2- посевом семян. После того, как женьшень посажен на землю, женьшень выращивают, поливая его различными удобрениями в течение 1-8 лет. Свежесобранный с земли влажный женьшень возрастом от 1 до 8 лет (инсам-инсам, сесамсусам по-корейски)... *картинка вставки* считается обычным женьшенем (но он также обладает многими полезными свойствами. Среди трав он занимает первое место после красный женьшень). Свежесобранный с земли женьшень в возрасте от 1 до 8 лет моется и очищается на фабрике, лишние ветки обрезаются, а женьшень высушивается с использованием специальной технологии сушки под давлением на фабрике (высушенный женьшень на Корейский, при высыхании краснеет и его полезные свойства еще больше возрастают, поэтому его достаточно пить и он считается королем трав в мире)* на картинке называется красный женьшень.",
+        },
+    ],
+    fakt:[
+        {
+            id:1,
+            lanUZ: "Faktlar sonlarda",
+            lanEN: "The facts are in the numbers",
+            lanRU: "Факты в цифрах",
+        },
+        {
+            id:2,
+            lanUZ: "100 ga yaqin kasalliklarni oldini olish va davolashda qollaniladi",
+            lanEN: "It is used in the prevention and treatment of about 100 diseases",
+            lanRU: "Применяется для профилактики и лечения около 100 заболеваний.",
+        },
+        {
+            id:3,
+            lanUZ: "1.3 mlrd jenshenni inson uchun eng foydali mahsulot deb hisoblashadi ",
+            lanEN: "1.3 billion ginseng is considered the most useful product for humans",
+            lanRU: "1,3 миллиарда женьшеня считают самым полезным продуктом для человека",
+        },
+        {
+            id:4,
+            lanUZ: "Jenshen o'simligi 100 yilgacha umr ko'radi",
+            lanEN: "The ginseng plant lives up to 100 years",
+            lanRU: "Женьшень живет до 100 лет",
+        }
+    ],
+    footer3:[
+        {
+            id:1,
+            lanUZ: "Janubiy Koreyada tayyorlangan haqiqiy 6 yillik qizil jenshen ekstrakti",
+            lanEN: "Authentic 6-year red ginseng extract made in South Korea",
+            lanRU: "Экстракт аутентичного красного женьшеня шестилетней выдержки производства Южной Кореи.",
+        },
+        {
+            id:2,
+            lanUZ: "Kompaniya haqida",
+            lanEN: "About the company",
+            lanRU: "О компании",
+        },
+        {
+            id:3,
+            lanUZ: "faktlar",
+            lanEN: "the facts",
+            lanRU: "факты",
+        },
+        {
+            id:4,
+            lanUZ: "mahsulot",
+            lanEN: "product",
+            lanRU: "продукт",
+        },
+        {
+            id:5,
+            lanUZ: "Formani toʻldiring, mutaxascislardan bepul konsultatsiya oling va chegirmaga ega boling",
+            lanEN: "Fill out the form, get a free consultation from specialists and get a discount",
+            lanRU: "Заполните форму, получите бесплатную консультацию специалистов и получите скидку",
+        },
+        {
+            id:6,
+            lanUZ: "Buyurtma berish",
+            lanEN: "Place an order",
+            lanRU: "Разместить заказ",
+        }
     ]
  }
 
--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 let elOrder = document.querySelector('.order')
 let localLong = window.localStorage.getItem('local')
+let str = ""
 if(localLong == 'UZ'||localLong == 'EN'|| localLong == 'RU'){
     str = `lan${localLong}`
 
@@ -7,6 +8,179 @@ if(localLong == 'UZ'||localLong == 'EN'|| localLong == 'RU'){
 else{
     str = "lanUZ"
 }
+let mAbaut = document.querySelector('.m_about')
+
+
+
+
+let Fakt = document.querySelector('.fakt')
+Fakt.innerHTML = `
+<div class="container">
+<div>
+<h3 class="F_TITLEF item1">${Data.fakt[0][str]}</h3>
+<img class="f_image" src="./img/Group 52 (1).png" alt="" />
+</div>
+<div class="f_p11">
+<p class="faktitem item2">
+  <span>100</span> ${Data.fakt[1][str]}
+</p>
+</div>
+<div class="f_p22">
+<p class="faktitem item1 ">
+  <span> 1.3 mlrd </span>${Data.fakt[2][str]}
+</p>
+</div>
+<div class="f_33">
+<p class="faktitem">
+  <span>100</span>${Data.fakt[2][str]}
+</p>
+</div>`
+
+let faktItem1 = document.querySelector('.item2')
+let fTitle = document.querySelector('.F_TITLEF')
+
+
+
+mAbaut.innerHTML = `
+<div class="container">
+  <div class="about">
+    <p class="a_text1">
+      ${Data.about[0][str]}
+    </p>
+    <img class="m_jen" src="./img/image 18.png" alt="" />
+    <p class="a_text2">${Data.about[0][str]}
+    </p>
+    <img class="m_jen2" src="./img/image 18 (1).png" alt="" />
+  </div>
+</div>`
+let aText1 = document.querySelector('.a_text1')
+let aText2 = document.querySelector('.a_text2')
+let Abaut2 = document.querySelector('.about2')
+Abaut2.innerHTML=`
+<div class="about2">
+  <div class="container">
+  <img class="a_img1" src="./img/Frame 15.png" alt="" />
+  <p class="global__text">${Data.footer2[1][str]}
+  </p>
+</div>
+</div>`
+
+
+let globalText = document.querySelector('.global__text')
+let main2 = document.querySelector('.main_2')
+main2.innerHTML = `
+<div class="container">
+<div class="main_bg">
+  <img class="main_img1" src="./img/iconhous.png" alt="" />
+  <img class="main_img2" src="./img/iconhous.png" alt="" />
+</div>
+<p class="m_p1">${Data.main[0][str]}</p>
+<p class="m_p2">${Data.main[1][str]}</p>
+</div>
+</div>`
+
+let mP1 = document.querySelector('.m_p1')
+let mP2 = document.querySelector('.m_p2')
+
+
+
+let abaut3 = document.querySelector('.about3')
+abaut3.innerHTML= `<div class="container">
+<div class="about3">
+  <img class="imgqasr" src="./img/Group 55.png" alt="" />
+  <h2 class="komp">${Data.about2[0][str]}</h2>
+  
+  <p class="soosum">${Data.about2[1][str]}
+  </p>
+</div>
+</div>`
+
+let Komp = document.querySelector('.komp')
+let soosumText = document.querySelector('.soosum')
+
+let FooterSec = document.querySelector('.footer')
+
+
+let Footer3 = document.querySelector('.footer3')
+
+Footer3.innerHTML= `
+<div>
+<p class="footer__text str1">${Data.footer3[0][str]}</p>
+</div>
+<div class="foter_ps">
+<p class="str2">${Data.footer3[1][str]}</p>
+<p class="str3">${Data.footer3[2][str]}</p>
+<p class="str4   ">${Data.footer3[3][str]}</p>
+</div>
+<div class="footer_inps">
+<h2 class="footerh2 str5">${Data.footer3[4][str]}</h2><br>
+<input class="inp" type="text" placeholder="username"><br><br>
+<input class="inp" type="number" placeholder="+998"><br>
+<button class="footerbtn str6">${Data.footer3[5][str]}</button>
+</div>`
+
+
+let Str1 = document.querySelector('.str1')
+let Str2 = document.querySelector('.str2')
+let Str3 = document.querySelector('.str3')
+let Str4 = document.querySelector('.str4')
+let Str5 = document.querySelector('.str5')
+let Str6 = document.querySelector('.str6')
+
+
+FooterSec.innerHTML= `
+<div class="container">
+<div class="footer">
+<img class="f_img" src="./img/Group 54.png" alt="" />
+<h2 class="f_text">${Data.footer[0][str]}
+</h2>
+<div class="f_p">
+  <p class="class__1">${Data.footer[1][str]}
+  </p>
+  <p  class="class__2">${Data.footer[2][str]}
+  </p>
+  <p class="class__3">${Data.footer[3][str]}
+  </p>
+  <p class="class__4">${Data.footer[4][str]}
+  </p>
+</div>
+<div class="f_p2">
+</p>
+  <p class="class__6">${Data.footer[6][str]}
+  </p>
+<p class="class__5">${Data.footer[5][str]}
+  <p class="class__7">${Data.footer[7][str]}
+  </p>
+</div>
+</div>
+</div>`
+let FText = document.querySelector('.f_text')
+let Class1 = document.querySelector('.class__1')
+let Class2 = document.querySelector('.class__2')
+let Class3 = document.querySelector('.class__3')
+let Class4 = document.querySelector('.class__4')
+let Class5 = document.querySelector('.class__5')
+let Class6 = document.querySelector('.class__6')
+let Class7 = document.querySelector('.class__7')
+let FooterNew = document.querySelector('.footer2')
+FooterNew.innerHTML= `
+<div class="container">
+<div class="footer_about">
+<img class="f_immg" src="./img/Group 53.png" alt="" />
+<h2 class="f_tite">${Data.footer2[0][str]}
+</h2>
+<p class="textFoter">${Data.footer2[1][str]}
+</p>
+</div>
+</div>`
+
+let Ftitle = document.querySelector('.f_tite')
+let Ftitle2 = document.querySelector('.textFoter')
+
+
+
+
+
 
 
 elOrder.innerHTML = ` <div class="box">
@@ -79,11 +253,24 @@ elSelect.addEventListener('change', (e) => {
 
 
     if(elSelect.value == "RU"){
+        Str1.textContent = Data.footer3[0].lanRU
+        Str2.textContent = Data.footer3[1].lanRU
+        Str3.textContent = Data.footer3[2].lanRU
+        Str4.textContent = Data.footer3[3].lanRU
+        Str5.textContent = Data.footer3[4].lanRU
+        Str6.textContent = Data.footer3[5].lanRU
         BoxTitle.textContent = Data.order[0].lanRU
         BoxDesc.textContent = Data.order[1].lanRU
         BoxInput.placeholder = Data.order[2].lanRU
         BoxBtn.textContent = Data.order[3].lanRU
         BoxH3.textContent = Data.order[4].lanRU
+        mP1.textContent =  Data.main[0].lanRU
+        mP2.textContent =  Data.main[1].lanRU
+        Komp.textContent = Data.about2[0].lanRU
+        Ftitle.textContent = Data.fakt[0].lanRU
+        Ftitle.textContent = Data.footer2[0].lanRU
+        Ftitle2.textContent = Data.footer2[1].lanRU
+        soosumText.textContent = Data.about2[1].lanRU
         BoxH1.textContent = Data.order[5].lanRU+ " 200 000"
         colorRed.classList='colorRed'
     // ----------------------------------------------------------
@@ -94,12 +281,32 @@ elSelect.addEventListener('change', (e) => {
         Sel2.textContent = Data.navbar[1].lanRU
         Sel3.textContent = Data.navbar[2].lanRU
         Sel4.value = Data.navbar[3].lanRU
+        aText1.textContent = Data.about[0].lanRU
+        aText2.textContent = Data.about[0].lanRU
+        globalText.textContent = Data.footer2[1].lanRU
+        FText.textContent = Data.footer[0].lanRU
+        Class1.textContent = Data.footer[0].lanRU
+        Class2.textContent = Data.footer[1].lanRU
+        Class3.textContent = Data.footer[2].lanRU
+        Class4.textContent = Data.footer[3].lanRU
+        Class5.textContent = Data.footer[4].lanRU
+        Class6.textContent = Data.footer[5].lanRU
+        Class7.textContent = Data.footer[6].lanRU
+        
         Sel4.classList.add('btn')
     }else if(elSelect.value == "UZ"){
         BoxTitle.textContent = Data.order[0].lanUZ
+        Str1.textContent = Data.footer3[0].lanUZ
+        Str2.textContent = Data.footer3[1].lanUZ
+        Str3.textContent = Data.footer3[2].lanUZ
+        Str4.textContent = Data.footer3[3].lanUZ
+        Str5.textContent = Data.footer3[4].lanUZ
+        Str6.textContent = Data.footer3[5].lanUZ
         BoxDesc.textContent = Data.order[1].lanUZ
         BoxInput.placeholder = Data.order[2].lanUZ
         BoxBtn.textContent = Data.order[3].lanUZ
+        FText.textContent = Data.footer[0].lanUZ
+
         BoxH3.textContent = Data.order[4].lanUZ
         BoxH1.textContent = Data.order[5].lanUZ+ " 200 000"
         colorRed.classList='colorRed'
@@ -108,8 +315,26 @@ elSelect.addEventListener('change', (e) => {
         heroBtn.textContent = Data.hero[2].lanUZ
         Sel1.textContent = Data.navbar[0].lanUZ
         Sel2.textContent = Data.navbar[1].lanUZ
+        Ftitle.textContent = Data.fakt[0].lanUZ
         Sel3.textContent = Data.navbar[2].lanUZ
         Sel4.textContent = Data.navbar[3].lanUZ
+        aText1.textContent = Data.about[0].lanUZ
+        aText2.textContent = Data.about[0].lanUZ
+        globalText.textContent = Data.footer2[1].lanUZ
+        mP1.textContent =  Data.main[0].lanUZ
+        mP2.textContent =  Data.main[1].lanUZ
+        Ftitle.textContent = Data.footer2[0].lanUZ
+        Ftitle2.textContent = Data.footer2[1].lanUZ
+        Komp.textContent = Data.about2[0].lanUZ
+        soosumText.textContent = Data.about2[1].lanUZ
+        Class1.textContent = Data.footer[0].lanUZ
+        Class2.textContent = Data.footer[1].lanUZ
+        Class3.textContent = Data.footer[2].lanUZ
+        Class4.textContent = Data.footer[3].lanUZ
+        Class5.textContent = Data.footer[4].lanUZ
+        Class6.textContent = Data.footer[5].lanUZ
+        Class7.textContent = Data.footer[6].lanUZ
+
         Sel4.classList.add('btn')
         }
     else if(elSelect.value == "EN"){
@@ -120,14 +345,39 @@ elSelect.addEventListener('change', (e) => {
         BoxH3.textContent = Data.order[4].lanEN
         BoxH1.textContent = Data.order[5].lanEN+ " 200 000"
         colorRed.classList='colorRed'
-
+        globalText.textContent = Data.footer2[1].lanEN
+        Komp.textContent = Data.about2[0].lanEN
+        soosumText.textContent = Data.about2[1].lanEN
         heroDesc.textContent = Data.hero[0].lanEN
         heroTitle.textContent = Data.hero[1].lanEN
+        Ftitle.textContent = Data.fakt[0].lanEN
+
         heroBtn.textContent = Data.hero[2].lanEN
         Sel1.textContent = Data.navbar[0].lanEN
         Sel2.textContent = Data.navbar[1].lanEN
+        FText.textContent = Data.footer[0].lanEN
+
         Sel3.textContent = Data.navbar[2].lanEN
         Sel4.textContent = Data.navbar[3].lanEN
+        aText1.textContent = Data.about[0].lanEN
+        aText2.textContent = Data.about[0].lanEN
+        mP1.textContent =  Data.main[0].lanEN
+        mP2.textContent =  Data.main[1].lanEN
+        Class1.textContent = Data.footer[0].lanEN
+        Class2.textContent = Data.footer[1].lanEN
+        Class3.textContent = Data.footer[2].lanEN
+        Str1.textContent = Data.footer3[0].lanEN
+        Str2.textContent = Data.footer3[1].lanEN
+        Str3.textContent = Data.footer3[2].lanEN
+        Str4.textContent = Data.footer3[3].lanEN
+        Str5.textContent = Data.footer3[4].lanEN
+        Str6.textContent = Data.footer3[5].lanEN
+        Class4.textContent = Data.footer[3].lanEN
+        Class5.textContent = Data.footer[4].lanEN
+        Class6.textContent = Data.footer[5].lanEN
+        Class7.textContent = Data.footer[6].lanEN
+        Ftitle.textContent = Data.footer2[0].lanEN
+        Ftitle2.textContent = Data.footer2[1].lanEN
         Sel4.classList.add('btn')
         }
     
